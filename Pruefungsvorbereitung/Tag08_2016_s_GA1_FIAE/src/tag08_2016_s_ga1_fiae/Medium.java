4c3c186b7f56e71ffbc70f94b2d809908900f864
Ragip Gashi <ragipgashi@gmail.com>
2020-10-14 16:10:22 +0200

    #1 Added another change to the file Tag08_2016_s_GA1_FIAE

--- Pruefungsvorbereitung/Tag08_2016_s_GA1_FIAE/src/tag08_2016_s_ga1_fiae/Medium.java
+++ Pruefungsvorbereitung/Tag08_2016_s_GA1_FIAE/src/tag08_2016_s_ga1_fiae/Medium.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tag08_2016_s_ga1_fiae;
 
 /**
@@ -10,5 +5,8 @@ package tag08_2016_s_ga1_fiae;
  * @author RagipGashi
  */
 public class Medium {
-    
+
+    int getBibNr() {
+        return 0;
+    }
 }
